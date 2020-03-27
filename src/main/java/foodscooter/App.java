d84b366e0f0ae58ecaba114560568e665c578317
Gabriel Yik Ren Jun <gabrielyik@yahoo.com.sg>
2020-03-27 22:58:29 +0800

    Disable CORS for port 4200

--- src/main/java/foodscooter/App.java
+++ src/main/java/foodscooter/App.java
@@ -2,10 +2,22 @@ package foodscooter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class App {
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
+  }
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/*").allowedOrigins("http://localhost:4200");
+      }
+    };
   }
 }
